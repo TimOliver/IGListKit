06060cb5b4618467db015d811401e1a64fb51559
Tim Oliver <git@tim.dev>
2023-03-11 15:57:08 +0900

    Added more description string tests

--- Tests/IGListDiffDescriptionStringTests.m
+++ Tests/IGListDiffDescriptionStringTests.m
@@ -12,8 +12,8 @@
 
 #import "IGListMoveIndexInternal.h"
 #import "IGListMoveIndexPathInternal.h"
-#import "IGListIndexPathResultInternal.h"
 #import "IGListIndexSetResultInternal.h"
+#import "IGListIndexPathResultInternal.h"
 
 @interface IGListDiffDescriptionStringTests : XCTestCase
 
@@ -48,6 +48,91 @@
                                                                 "insertIndexPaths: 1; "
                                                                 "updateIndexPaths: 0>", result];
     XCTAssertTrue([result.description isEqualToString:expectedDescription]);
+}
+
+- (void)test_withIndexPathResult_thatDescriptionStringIsValid {
+    NSArray *inserts = @[[NSIndexPath indexPathForRow:0 inSection:0]];
+    NSArray *deletes = @[
+        [NSIndexPath indexPathForRow:0 inSection:1],
+        [NSIndexPath indexPathForRow:1 inSection:1]
+    ];
+    NSArray *updates = @[
+        [NSIndexPath indexPathForRow:1 inSection:0]
+    ];
+    NSArray *moves = @[
+        [[IGListMoveIndexPath alloc] initWithFrom:[NSIndexPath indexPathForRow:1 inSection:3]
+                                               to:[NSIndexPath indexPathForRow:2 inSection:3]],
+        [[IGListMoveIndexPath alloc] initWithFrom:[NSIndexPath indexPathForRow:4 inSection:3]
+                                               to:[NSIndexPath indexPathForRow:3 inSection:3]]
+    ];
+    
+    IGListIndexPathResult *result = [[IGListIndexPathResult alloc] initWithInserts:inserts
+                                                                           deletes:deletes
+                                                                           updates:updates
+                                                                             moves:moves
+                                                                   oldIndexPathMap:[NSMapTable mapTableWithKeyOptions:0 valueOptions:0]
+                                                                   newIndexPathMap:[NSMapTable mapTableWithKeyOptions:0 valueOptions:0]];
+    
+    NSString *expectedDescription = [NSString stringWithFormat:@"<IGListIndexPathResult %p; "
+                                                                "1 inserts; "
+                                                                "2 deletes; "
+                                                                "1 updates; "
+                                                                "2 moves>", result];
+    XCTAssertTrue([result.description isEqualToString:expectedDescription]);
+}
+
+- (void)test_withIndexSetResult_thatDescriptionStringIsValid {
+    NSMutableIndexSet *inserts = [NSMutableIndexSet indexSet];
+    [inserts addIndex:0];
+    [inserts addIndex:1];
+    
+    NSMutableIndexSet *deletes = [NSMutableIndexSet indexSet];
+    [deletes addIndex:3];
+    
+    NSMutableIndexSet *updates = [NSMutableIndexSet indexSet];
+    [updates addIndex:4];
+    [updates addIndex:5];
+    [updates addIndex:6];
+    
+    NSArray *moves = @[
+        [[IGListMoveIndex alloc] initWithFrom:9 to:10],
+        [[IGListMoveIndex alloc] initWithFrom:11 to:12]
+    ];
+    
+    IGListIndexSetResult *result = [[IGListIndexSetResult alloc] initWithInserts:inserts
+                                                                         deletes:deletes
+                                                                         updates:updates
+                                                                           moves:moves
+                                                                     oldIndexMap:[NSMapTable mapTableWithKeyOptions:0 valueOptions:0]
+                                                                     newIndexMap:[NSMapTable mapTableWithKeyOptions:0 valueOptions:0]];
+
+    NSString *expectedDescription = [NSString stringWithFormat:@"<IGListIndexSetResult %p; "
+                                                                "2 inserts; "
+                                                                "1 deletes; "
+                                                                "3 updates; "
+                                                                "2 moves>", result];
+    XCTAssertTrue([result.description isEqualToString:expectedDescription]);
+}
+
+- (void)test_withMoveIndex_thatDescriptionStringIsValid {
+    IGListMoveIndex *moveIndex = [[IGListMoveIndex alloc] initWithFrom:3 to:4];
+
+    NSString *expectedDescription = [NSString stringWithFormat:@"<IGListMoveIndex %p; "
+                                                                "from: 3; "
+                                                                "to: 4;>", moveIndex];
+    XCTAssertTrue([moveIndex.description isEqualToString:expectedDescription]);
+}
+
+- (void)test_withMoveIndexPath_thatDescriptionStringIsValid {
+    NSIndexPath *from = [NSIndexPath indexPathForRow:1 inSection:1];
+    NSIndexPath *to = [NSIndexPath indexPathForRow:3 inSection:1];
+    IGListMoveIndexPath *moveIndexPath = [[IGListMoveIndexPath alloc] initWithFrom:from to:to];
+
+    NSString *expectedDescription = [NSString stringWithFormat:@"<IGListMoveIndexPath %p; "
+                                                                 "from: <NSIndexPath: %p> {length = 2, path = 1 - 1}; "
+                                                                 "to: <NSIndexPath: %p> {length = 2, path = 1 - 3};>",
+                                     moveIndexPath, from, to];
+    XCTAssertTrue([moveIndexPath.description isEqualToString:expectedDescription]);
 }
 
 @end
